1487b6b1c1754c6ce9e96c60b44c311ce1d5f062
tapia1013 <manbearpig1013@gmail.com>
2021-11-05 11:48:53 -0700

    installed bcrypt and setup seeder

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs'
+
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true
+  },
+  {
+    name: 'Cookie Tapia',
+    email: 'cookie@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Nye Rodriguez',
+    email: 'bye@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+]
+
+export default users

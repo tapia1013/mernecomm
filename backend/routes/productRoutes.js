5e9f5b3c8541d8271c41d2b2aef56e9c53b0a4b2
tapia1013 <manbearpig1013@gmail.com>
2021-11-05 20:15:41 -0700

    added controller to backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,45 +1,15 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
+// import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+// import Product from '../models/productModel.js'
+
+import { getProducts, getProductById } from '../controllers/productController.js';
 
 
 
+router.route('/').get(getProducts)
 
-
-
-
-// @desc      Fetch all products
-// @route     GET /api/products
-// @access    Public
-// asyncHandler is for us not having to try{}catch(){} everything
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({})
-
-  res.json(products)
-}))
-
-
-
-
-// @desc      Fetch single product
-// @route     GET /api/products/:id
-// @access    Public
-router.get('/:id', asyncHandler(async (req, res) => {
-  // if theres an error with id ue ._id
-  const product = await Product.findById(req.params.id)
-
-  // check if theres a product
-  if (product) {
-    res.json(product)
-  } else {
-    res.status(404)
-    throw new Error('Product not found')
-  }
-
-}))
-
-
+router.route('/:id').get(getProductById)
 
 
 

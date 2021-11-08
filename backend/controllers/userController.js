78fe18a7e1edcb0ecb848d46601ca0d714064e58
tapia1013 <manbearpig1013@gmail.com>
2021-11-08 14:00:22 -0800

    register, update user, login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -83,7 +83,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route      GET /api/users/profile
 // @access     Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  const user = User.findById(req.user._id);
+  const user = await User.findById(req.user._id);
 
   if (user) {
     res.json({
@@ -108,4 +108,46 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 
 
-export { authUser, getUserProfile, registerUser };
+
+
+/**        update profile      */
+// @desc       Update user profile
+// @route      PUT /api/users/profile
+// @access     Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    // if user true
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id)
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+
+
+});
+
+
+
+
+
+
+
+
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,7 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -12,8 +13,8 @@ import { protect } from '../middleware/authMiddleware.js'
 
 
 router.route('/').post(registerUser)
-router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile);
+router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 
 

e7499920228b7ee667016483c0811ca49277ca7c
tapia1013 <manbearpig1013@gmail.com>
2021-11-08 15:55:22 -0800

    update profile but still have issues getting the profile data

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,57 +1,152 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
-
-
-
-// define all the fields we want for the user
 const userSchema = mongoose.Schema(
   {
-    // since we want required we name: {type:string}
     name: {
       type: String,
-      required: true
+      required: true,
     },
     email: {
       type: String,
       required: true,
-      unique: true
+      unique: true,
     },
     password: {
       type: String,
-      required: true
+      required: true,
     },
     isAdmin: {
       type: Boolean,
       required: true,
-      default: false
+      default: false,
     },
   },
   {
-    timeStamps: true
+    timestamps: true,
   }
 )
 
-// password authentication with plaintext pw and match it with the bcrypt pw that is hashed
-userSchema.methods.matchPasswords = async function (enteredPassword) {
-  // compare let us cmpare plain text pw to bcrypt hashed pw
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
-
-// before we save encrypt
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
-    next();
+    next()
   }
 
-  // encrypt here
-  const salt = await bcrypt.genSalt(10);
-  // hash
-  this.password = await bcrypt.hash(this.password, salt);
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
 })
-
 
 const User = mongoose.model('User', userSchema)
 
-export default User;
+export default User
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from 'mongoose';
+// import bcrypt from 'bcryptjs';
+
+
+
+
+// // define all the fields we want for the user
+// const userSchema = mongoose.Schema(
+//   {
+//     // since we want required we name: {type:string}
+//     name: {
+//       type: String,
+//       required: true
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true
+//     },
+//     password: {
+//       type: String,
+//       required: true
+//     },
+//     isAdmin: {
+//       type: Boolean,
+//       required: true,
+//       default: false
+//     },
+//   },
+//   {
+//     timestamps: true
+//   }
+// )
+
+// // password authentication with plaintext pw and match it with the bcrypt pw that is hashed
+// userSchema.methods.matchPasswords = async function (enteredPassword) {
+//   // compare let us cmpare plain text pw to bcrypt hashed pw
+//   return await bcrypt.compare(enteredPassword, this.password)
+// }
+
+
+// // before we save encrypt
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) {
+//     next();
+//   }
+
+//   // encrypt here
+//   const salt = await bcrypt.genSalt(10);
+//   // hash
+//   this.password = await bcrypt.hash(this.password, salt);
+// })
+
+
+// const User = mongoose.model('User', userSchema)
+
+// export default User;
+
+
